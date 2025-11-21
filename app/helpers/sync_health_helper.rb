cc5757c32e3c5640f9133ff5b0ba456488b971e6
Hendri Permana <hendri@permana.icu>
2025-11-21 10:00:17 +0700

    Reduce sync health cache TTL for fresher banner

--- app/helpers/sync_health_helper.rb
+++ app/helpers/sync_health_helper.rb
@@ -4,7 +4,7 @@ module SyncHealthHelper
     return {} unless fam
 
     # Cache briefly to avoid heavy queries on dashboards
-    Rails.cache.fetch(fam.build_cache_key("sync_health_banner"), expires_in: 1.minute) do
+    Rails.cache.fetch(fam.build_cache_key("sync_health_banner"), expires_in: 30.seconds) do
       account_ids = fam.accounts.pluck(:id)
       scope = Sync.where(
         "(syncable_type = ? AND syncable_id = ?) OR (syncable_type = ? AND syncable_id IN (?))",
